47d8c66e90e4420cb8c8f8b27ee97474b75dd333
huangp0772 <huangp0772@CA-SU-F106-16.sduhsd.lan>
2020-02-20 11:11:28 -0800

    Working on it

--- APCSA2020/src/Unit7/RPSRunner.java
+++ APCSA2020/src/Unit7/RPSRunner.java
@@ -4,6 +4,7 @@ package Unit7;
 //Name -  
 
 import java.util.Scanner;
+import java.util.Random;
 import static java.lang.System.*;
 
 public class RPSRunner
@@ -11,17 +12,36 @@ public class RPSRunner
 	public static void main(String args[])
 	{
 		Scanner keyboard = new Scanner(System.in);
-		char response;
+		Random rand = new Random();
 		
+		
+		char response = 'y';
+		String playerWeapon;
+		String CPUWeapon;
 		//add in a do while loop after you get the basics up and running
-		
+		while(response=='y') {
 			String player = "";
-		
-			out.print("type in your prompt [R,P,S] :: ");
-			
+			playerWeapon = keyboard.nextLine();
+			out.print("Rock-Paper-Scissors - pick your weapon[R,P,S] :: " + playerWeapon);
+			out.println("player had " + playerWeapon);
+			int x = rand.nextInt(2);
+			if(x==0) {
+				CPUWeapon = "R";
+			} else if(x==1) {
+				CPUWeapon = "P";
+			} else {
+				CPUWeapon = "S";
+			}
+			out.println("computer has " + CPUWeapon);
 			//read in the player value
-		
-			RockPaperScissors game = new RockPaperScissors();		
+			RockPaperScissors game = new RockPaperScissors();	
+			game.setPlayers(playerWeapon);
+			game.setComp(CPUWeapon);
+			
+			
+			System.out.print("Do you want to play again? ");
+			response = keyboard.next().charAt(0);
+		}
 	}
 }
 
--- APCSA2020/src/Unit7/RockPaperScissors.java
+++ APCSA2020/src/Unit7/RockPaperScissors.java
@@ -3,6 +3,7 @@ package Unit7;
 // www.apluscompsci.com
 //Name -  
 
+import java.util.Random;
 import java.util.Scanner;
 import static java.lang.System.*;
 
@@ -10,22 +11,48 @@ public class RockPaperScissors
 {
 	private String playChoice;
 	private String compChoice;
-
+	
 	public RockPaperScissors()
 	{
 	}
 
 	public RockPaperScissors(String player)
 	{
+		
 	}
 
 	public void setPlayers(String player)
 	{
+		playChoice = player;
 	}
-
+	
+	public void setComp(String computer) 
+	{
+		compChoice = computer;
+	}
+	
 	public String determineWinner()
 	{
 		String winner="";
+		if(playChoice.equals("R")) {
+			if(compChoice.equals("P")) {
+				winner = "Computer";
+			} else if(compChoice.equals("S")) {
+				winner = "Player";
+			}
+		} else if(playChoice.equals("P")) {
+			if(compChoice.equals("S")) {
+				winner = "Computer";
+			} else if(compChoice.equals("R")) {
+				winner = "Player";
+			}
+		} else if(playChoice.equals("S")) {
+			if(compChoice.equals("R")) {
+				winner = "Computer";
+			} else if(compChoice.equals("P")) {
+				winner = "Player";
+			}
+		}
 		return winner;
 	}
 
