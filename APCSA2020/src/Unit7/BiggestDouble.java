4b0135bf7043081ca0d4cf3eb058b6d3f5ce40f6
huangp0772 <huangp0772@CA-SU-F106-16.sduhsd.lan>
2020-02-20 10:11:30 -0800

    finished

--- APCSA2020/src/Unit7/BiggestDouble.java
+++ APCSA2020/src/Unit7/BiggestDouble.java
@@ -16,19 +16,55 @@ public class BiggestDouble
 
 	public BiggestDouble(double a, double b, double c, double d)
 	{
+		one=a;
+		two=b;
+		three=c;
+		four=d;
 	}
 
 	public void setDoubles(double a, double b, double c, double d)
 	{
+		one=a;
+		two=b;
+		three=c;
+		four=d;
 	}
 
 	public double getBiggest()
 	{
-		return 0.0;
+		double max = 0;
+		for(int i = 0; i < 3; i++) {
+			if(one>=two) {
+				if(one>=three) {
+					if(one>=four) {
+						max = one;
+					}else {
+						max = four;
+					}
+				} else if(three>=four) {
+					max = three;
+				}
+			} else if(two>=three) {
+				if(two>=four) {
+					max = two;
+				} else {
+					max = four;
+				}
+			} else if(four>=three) {
+				if(four>=two) {
+					max = four;
+				} else {
+					max = two;
+				}
+			}
+		}
+		
+		
+		return max;
 	}
 
 	public String toString()
 	{
-	   return "";
+	   return one+" "+two+" "+three+" "+four;
 	}
 }
--- APCSA2020/src/Unit7/DoubleRunner.java
+++ APCSA2020/src/Unit7/DoubleRunner.java
@@ -9,11 +9,50 @@ public class DoubleRunner
 {
 	public static void main(String[] args)
 	{
-		BiggestDouble run = new BiggestDouble(4.5,6.7,7.8,9.0);
-		System.out.println(run);
-		System.out.println("biggest = " + run.getBiggest() + "\n");
-
+		BiggestDouble run1 = new BiggestDouble(4.5,6.7,7.8,9.0);
+		System.out.println(run1.toString());
+		System.out.println("biggest = " + run1.getBiggest() + "\n");
+		
+		BiggestDouble run2 = new BiggestDouble(14.51,6.17,71.8,1.0);
+		System.out.println(run2.toString());
+		System.out.println("biggest = " + run2.getBiggest() + "\n");
+		
+		BiggestDouble run3 = new BiggestDouble(41.15,816.7,17.8,19.0);
+		System.out.println(run3.toString());
+		System.out.println("biggest = " + run3.getBiggest() + "\n");
+		
+		BiggestDouble run4 = new BiggestDouble(884.5,16.7,7.8,9.0);
+		System.out.println(run4.toString());
+		System.out.println("biggest = " + run4.getBiggest() + "\n");
+		
+		BiggestDouble run5 = new BiggestDouble(4.5,-456.7,677.8,9.0);
+		System.out.println(run5.toString());
+		System.out.println("biggest = " + run5.getBiggest() + "\n");
+		
+		BiggestDouble run6 = new BiggestDouble(4.5,16.7,-7.8,-9.0);
+		System.out.println(run6.toString());
+		System.out.println("biggest = " + run6.getBiggest() + "\n");
 		//add more test cases
 				
 	}
 }
+/*---------------------------------result--------------------------------------
+4.5 6.7 7.8 9.0
+biggest = 9.0
+
+14.51 6.17 71.8 1.0
+biggest = 71.8
+
+41.15 816.7 17.8 19.0
+biggest = 816.7
+
+884.5 16.7 7.8 9.0
+biggest = 884.5
+
+4.5 -456.7 677.8 9.0
+biggest = 677.8
+
+4.5 16.7 -7.8 -9.0
+biggest = 16.7
+
+*/
