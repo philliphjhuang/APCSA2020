e85f2eca343caf58445d21627178a16d95538f12
huangp0772 <huangp0772@CA-SU-F106-16.sduhsd.lan>
2020-02-21 10:48:25 -0800

    finished

--- APCSA2020/src/Unit7/Triples.java
+++ APCSA2020/src/Unit7/Triples.java
@@ -1,49 +1,53 @@
 package Unit7;
 //(c) A+ Computer Science
+
 // www.apluscompsci.com
 //Name -  
 
 import static java.lang.System.*;
 
-public class Triples
-{
-   private int number;
+public class Triples {
+	private int number;
 
-	public Triples()
-	{
+	public Triples() {
 		this(0);
 	}
 
-	public Triples(int num)
-	{
-
-
+	public Triples(int num) {
+		number = num;
 	}
 
-	public void setNum(int num)
-	{
-
-
+	public void setNum(int num) {
+		number = num;
 	}
 	
-	private int greatestCommonFactor(int a, int b, int c)
-	{
-		int max = 0;
-
-
-
-		return 1;
+	public int greatestCommonFactor(int a, int b, int c) {
+		int max = 2;
+		for(int i = 1; i < number; i++) {
+			if((a%i) == 0&& (b%i)==0 && (c%i)==0) {
+				max = i;
+			}
+		}
+		return max;
 	}
 
-	public String toString()
-	{
-		String output="";
+	public String toString() {
+		String output = "";
 
+		for (int a = 1; a < 66; a++) {
+			for (int b = 4; b < number; b++) {
+				for (int c = 1; c < number; c += 2) {
+					if ((a * a + b * b) == c * c) {
+						if (a % 2 != b % 2) {
+							if (greatestCommonFactor(a, b, c) <= 1) {
+								output = output + a + " " + b + " " + c + "\n";
+							}
+						}
+					}
+				}
+			}
+		}
 
-
-
-
-
-		return output+"\n";
+		return output + "\n";
 	}
 }
