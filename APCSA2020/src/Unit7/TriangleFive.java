60b28c59457f2ca7f466cbbe6186cbc614a153d2
huangp0772 <huangp0772@CA-SU-F106-16.sduhsd.lan>
2020-02-24 10:51:40 -0800

    finished

--- APCSA2020/src/Unit7/TriangleFive.java
+++ APCSA2020/src/Unit7/TriangleFive.java
@@ -1,34 +1,55 @@
 package Unit7;
 //(c) A+ Computer Science
+
 //www.apluscompsci.com
 //Name -
 
 import static java.lang.System.*;
 
-public class TriangleFive
-{
-   private char letter;
-   private int amount;
+public class TriangleFive {
+	private char letter;
+	private int amount;
 
-	public TriangleFive()
-	{
+	public TriangleFive() {
 	}
 
-	public TriangleFive(char c, int amt)
-	{
+	public TriangleFive(char c, int amt) {
+		letter = c;
+		amount = amt;
 	}
 
-	public void setLetter(char c)
-	{
+	public void setLetter(char c) {
+		letter = c;
 	}
 
-	public void setAmount(int amt)
-	{
+	public void setAmount(int amt) {
+		amount = amt;
 	}
 
-	public String toString()
-	{
-		String output="";
+	public String toString() {
+		String output = "";
+		String output1 = "";
+		char letter1 = letter;
+		for (int i = amount; i > 0; i--) {
+			int j = i;
+			while (j > 0) {
+				output = output + letter1;
+				j--;
+			}
+			output += " ";
+			if (letter1 != 90) {
+				letter1++;
+			} else {
+				letter1 = 'A';
+			}
+		}
+		output1 = output;
+		int val = 2, change = 3;
+		for (int i = amount; i > 0; i--) {
+			output = output + "\n" + output1.substring(0, output1.length() - val);
+			val += change;
+			change += 1;
+		}
 		return output;
 	}
 }
--- APCSA2020/src/Unit7/TriangleFiveRunner.java
+++ APCSA2020/src/Unit7/TriangleFiveRunner.java
@@ -9,5 +9,58 @@ public class TriangleFiveRunner
 {
    public static void main(String args[])
    {
-	}
+	   TriangleFive tf = new TriangleFive();
+	   tf.setLetter('C');
+	   tf.setAmount(4);
+	   System.out.println(tf.toString());
+	   tf.setLetter('A');
+	   tf.setAmount(5);
+	   System.out.println(tf.toString());
+	   tf.setLetter('B');
+	   tf.setAmount(7);
+	   System.out.println(tf.toString());
+	   tf.setLetter('X');
+	   tf.setAmount(6);
+	   System.out.println(tf.toString());
+	   tf.setLetter('Z');
+	   tf.setAmount(8);
+	   System.out.println(tf.toString());
+   }
 }
+/*---------------------------------------------result----------------------------------------
+CCCC DDD EE F 
+CCCC DDD EE 
+CCCC DDD 
+CCCC 
+
+AAAAA BBBB CCC DD E 
+AAAAA BBBB CCC DD 
+AAAAA BBBB CCC 
+AAAAA BBBB 
+AAAAA 
+
+BBBBBBB CCCCCC DDDDD EEEE FFF GG H 
+BBBBBBB CCCCCC DDDDD EEEE FFF GG 
+BBBBBBB CCCCCC DDDDD EEEE FFF 
+BBBBBBB CCCCCC DDDDD EEEE 
+BBBBBBB CCCCCC DDDDD 
+BBBBBBB CCCCCC 
+BBBBBBB 
+
+XXXXXX YYYYY ZZZZ AAA BB C 
+XXXXXX YYYYY ZZZZ AAA BB 
+XXXXXX YYYYY ZZZZ AAA 
+XXXXXX YYYYY ZZZZ 
+XXXXXX YYYYY 
+XXXXXX 
+
+ZZZZZZZZ AAAAAAA BBBBBB CCCCC DDDD EEE FF G 
+ZZZZZZZZ AAAAAAA BBBBBB CCCCC DDDD EEE FF 
+ZZZZZZZZ AAAAAAA BBBBBB CCCCC DDDD EEE 
+ZZZZZZZZ AAAAAAA BBBBBB CCCCC DDDD 
+ZZZZZZZZ AAAAAAA BBBBBB CCCCC 
+ZZZZZZZZ AAAAAAA BBBBBB 
+ZZZZZZZZ AAAAAAA 
+ZZZZZZZZ 
+
+*/
