3140f86234819b4600521a116e472d417ddbf3f9
huangp0772 <huangp0772@CA-SU-F106-16.sduhsd.lan>
2020-02-24 11:02:29 -0800

    initialized

---
+++ APCSA2020/src/elevensLab/Card.java
@@ -0,0 +1,28 @@
+package elevensLab;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+public class Card
+{
+	public static final String FACES[] = {"ZERO","ACE","TWO","THREE","FOUR",
+			"FIVE","SIX","SEVEN","EIGHT","NINE","TEN","JACK","QUEEN","KING"};
+
+	//instance variables
+		//String suit
+		//int face
+
+  	//constructors
+
+
+	// modifiers
+		//set methods
+
+
+  	//accessors
+		//get methods
+
+
+  	//toString
+
+ }
