5bc3d9ff08fb390c94ef4f9d5549526f2676bc75
huangp0772 <huangp0772@CA-SU-F106-16.sduhsd.lan>
2020-02-26 11:49:01 -0800

    practice(not part of homework)

--- APCSA2020/src/Unit8/FirstRunner.java
+++ APCSA2020/src/Unit8/FirstRunner.java
@@ -10,7 +10,7 @@ public class FirstRunner
 	{
 		
 		RayFirst rf = new RayFirst();
-		
+		 
 		System.out.println( rf.go( new int[]{-99,1,2,3,4,5,6,7,8,9,10,12345} ) );
 		System.out.println( rf.go( new int[]{10,9,8,7,6,5,4,3,2,1,-99} ) );
 		System.out.println( rf.go( new int[]{10,20,30,40,50,-11818,40,30,20,10} ) );
