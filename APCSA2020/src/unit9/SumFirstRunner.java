a6419b39dee5c0749412922d137ba98143733f38
huangp0772 <huangp0772@CA-SU-F106-16.sduhsd.lan>
2020-02-26 11:14:20 -0800

    work in progress

--- APCSA2020/src/unit9/SumFirstRunner.java
+++ APCSA2020/src/unit9/SumFirstRunner.java
@@ -1,4 +1,8 @@
 package unit9;
+
+import java.awt.List;
+import java.util.ArrayList;
+
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
@@ -7,6 +11,21 @@ package unit9;
 public class SumFirstRunner
 {
 	public static void main( String args[] )
-	{										
+	{			
+		ListSumFirst lsf = new ListSumFirst();
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		
+		System.out.println( lsf.go( List.of(-99,1,2,3,4,5,6,7,8,9,10,5) ) );
+		System.out.println( lsf.go( new int[]{10,9,8,7,6,5,4,3,2,1,-99} ) );
+		System.out.println( lsf.go( new int[]{10,20,30,40,50,-11818,40,30,20,10} ) );
+		System.out.println( lsf.go( new int[]{32767} ) );
+		System.out.println( lsf.go( new int[]{255,255} ) );
+		System.out.println( lsf.go( new int[]{9,10,-88,100,-555,2} ) );
+		System.out.println( lsf.go( new int[]{10,10,10,11,456} ) );
+		System.out.println( lsf.go( new int[]{-111,1,2,3,9,11,20,1} ) );
+		System.out.println( lsf.go( new int[]{9,8,7,6,5,4,3,2,0,-2,6} ) );
+		System.out.println( lsf.go( new int[]{12,15,18,21,23,1000} ) );
+		System.out.println( lsf.go( new int[]{250,19,17,15,13,11,10,9,6,3,2,1,0} ) );	
+		System.out.println( lsf.go( new int[]{9,10,-8,10000,-5000,-3000} ) );
 	}
 }
