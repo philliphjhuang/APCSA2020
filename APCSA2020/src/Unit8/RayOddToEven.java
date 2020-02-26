4b5fc547a1f9ef062106020cf3d4cab1d63105de
huangp0772 <huangp0772@CA-SU-F106-16.sduhsd.lan>
2020-02-26 11:34:21 -0800

    finished

--- APCSA2020/src/Unit8/RayOddToEven.java
+++ APCSA2020/src/Unit8/RayOddToEven.java
@@ -6,8 +6,17 @@ package Unit8;
 
 public class RayOddToEven
 {
-	public static boolean go(int[] ray)
+	public static int go(int[] ray)
 	{
-		return false;
+		for(int i = 0; i < ray.length; i++) {
+			if((ray[i]%2)==1) {
+				for(int j = i; j < ray.length; j++) {
+					if((ray[j]%2)==0) {
+						return j-i;
+					}
+				}
+			}
+		}
+		return -1;
 	}
 }
