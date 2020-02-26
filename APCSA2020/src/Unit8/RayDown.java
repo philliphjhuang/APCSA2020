93a0f79005aa3b996574d5ea8de27f5a2524d1bc
huangp0772 <huangp0772@CA-SU-F106-16.sduhsd.lan>
2020-02-26 11:38:10 -0800

    initialized

--- APCSA2020/src/Unit8/RayDown.java
+++ APCSA2020/src/Unit8/RayDown.java
@@ -9,7 +9,7 @@ public class RayDown
 	//go() will return true if all numbers in numArray
 	//are in decreasing order [31,12,6,2,1]
 	public static boolean go(int[] numArray)
-	{
+	{ 
 		return true;
 	}	
 }
