39b87c3c3a6dd3aae1b6590d6f74a9c171bf5cf3
huangp0772 <huangp0772@CA-SU-F106-16.sduhsd.lan>
2020-02-27 10:18:17 -0800

    finished

--- APCSA2020/src/unit9/DownRunner.java
+++ APCSA2020/src/unit9/DownRunner.java
@@ -4,9 +4,35 @@ package unit9;
 //Name -
 //Date -
 
+import java.util.Arrays;
+
 public class DownRunner
 {
 	public static void main( String args[] )
-	{											
+	{		
+		Integer[] one = {-99,1,2,3,4,5,6,7,8,9,10,12345};
+		System.out.println(ListDown.go(Arrays.asList(one)));
+		Integer[] two = {10,9,8,7,6,5,4,3,2,1,-99};
+		System.out.println(ListDown.go(Arrays.asList(two)));
+		Integer[] three = {10,20,30,40,50,-11818,40,30,20,10};
+		System.out.println(ListDown.go(Arrays.asList(three)));
+		Integer[] four = {32767};
+		System.out.println(ListDown.go(Arrays.asList(four)));
+		Integer[] five = {255,255};
+		System.out.println(ListDown.go(Arrays.asList(five)));
+		Integer[] six = {9,10,-88,100,-555,1000};
+		System.out.println(ListDown.go(Arrays.asList(six)));
+		Integer[] seven = {10,10,10,11,456};
+		System.out.println(ListDown.go(Arrays.asList(seven)));
+		Integer[] eight = {-111,1,2,3,9,11,20,30};
+		System.out.println(ListDown.go(Arrays.asList(eight)));
+		Integer[] nine = {9,8,7,6,5,4,3,2,0,-2,-989};
+		System.out.println(ListDown.go(Arrays.asList(nine)));
+		Integer[] ten = {12,15,18,21,23,1000};
+		System.out.println(ListDown.go(Arrays.asList(ten)));
+		Integer[] eleven = {250,19,17,15,13,11,10,9,6,3,2,1,-455};
+		System.out.println(ListDown.go(Arrays.asList(eleven)));
+		Integer[] tweleve = {9,10,-8,10000,-5000,1000};
+		System.out.println(ListDown.go(Arrays.asList(tweleve)));
 	}
 }
--- APCSA2020/src/unit9/ListDown.java
+++ APCSA2020/src/unit9/ListDown.java
@@ -13,6 +13,14 @@ public class ListDown
 	//are in decreasing order [31,12,6,2,1]
 	public static boolean go(List<Integer> numArray)
 	{
-		return true;
+		boolean isDecreasing = false;
+		for(int i = 0; i < numArray.size()-1; i++) {
+			if(numArray.get(i) > numArray.get(i+1)) {
+				isDecreasing = true;
+			} else {
+				return false;
+			}
+		}
+		return isDecreasing;
 	}	
 }
