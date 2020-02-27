43bee59ab485fd46d26e3908214f91d697750abe
huangp0772 <huangp0772@CA-SU-F106-16.sduhsd.lan>
2020-02-27 10:09:30 -0800

    finished

--- APCSA2020/src/unit9/ListOddToEven.java
+++ APCSA2020/src/unit9/ListOddToEven.java
@@ -9,8 +9,24 @@ import java.util.List;
 
 public class ListOddToEven
 {
-	public static boolean go( List<Integer> ray )
+	public static int go( List<Integer> ray )
 	{
-		return false;
+		int firstOddPosition = -1;
+		int lastEvenPosition = -1;
+		for(int i = 0; i < ray.size(); i++) {
+			if((ray.get(i) % 2) == 1) {
+				firstOddPosition = i;
+				for(int j = i; j < ray.size(); j++) {
+					if((ray.get(j) % 2) == 0) {
+						lastEvenPosition = j;
+					}
+				}
+				break;
+			}
+		}
+		if(firstOddPosition == -1 || lastEvenPosition == -1) {
+			return -1;
+		}
+		return lastEvenPosition - firstOddPosition;
 	}
 }
