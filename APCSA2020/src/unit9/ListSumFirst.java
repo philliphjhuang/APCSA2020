80311b5bff768e37253528e172762371615818c0
huangp0772 <huangp0772@CA-SU-F106-16.sduhsd.lan>
2020-02-27 09:56:03 -0800

    finished

--- APCSA2020/src/unit9/ListSumFirst.java
+++ APCSA2020/src/unit9/ListSumFirst.java
@@ -11,6 +11,15 @@ public class ListSumFirst
 {
 	public static int go(List<Integer> ray)
 	{
-		return 0;
+		int sum = 0;
+		for(int i = 1; i < ray.size(); i++) {
+			if(ray.get(i)>ray.get(0)) {
+				sum += ray.get(i);
+			}
+		}
+		if(sum==0) {
+			return -1;
+		}
+		return sum;
 	}
 }
--- APCSA2020/src/unit9/SumFirstRunner.java
+++ APCSA2020/src/unit9/SumFirstRunner.java
@@ -2,6 +2,7 @@ package unit9;
 
 import java.awt.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 //(c) A+ Computer Science
 //www.apluscompsci.com
@@ -12,20 +13,29 @@ public class SumFirstRunner
 {
 	public static void main( String args[] )
 	{			
-		ListSumFirst lsf = new ListSumFirst();
-		ArrayList<Integer> al = new ArrayList<Integer>();
-		
-		System.out.println( lsf.go( List.of(-99,1,2,3,4,5,6,7,8,9,10,5) ) );
-		System.out.println( lsf.go( new int[]{10,9,8,7,6,5,4,3,2,1,-99} ) );
-		System.out.println( lsf.go( new int[]{10,20,30,40,50,-11818,40,30,20,10} ) );
-		System.out.println( lsf.go( new int[]{32767} ) );
-		System.out.println( lsf.go( new int[]{255,255} ) );
-		System.out.println( lsf.go( new int[]{9,10,-88,100,-555,2} ) );
-		System.out.println( lsf.go( new int[]{10,10,10,11,456} ) );
-		System.out.println( lsf.go( new int[]{-111,1,2,3,9,11,20,1} ) );
-		System.out.println( lsf.go( new int[]{9,8,7,6,5,4,3,2,0,-2,6} ) );
-		System.out.println( lsf.go( new int[]{12,15,18,21,23,1000} ) );
-		System.out.println( lsf.go( new int[]{250,19,17,15,13,11,10,9,6,3,2,1,0} ) );	
-		System.out.println( lsf.go( new int[]{9,10,-8,10000,-5000,-3000} ) );
+		Integer[] one = {-99,1,2,3,4,5,6,7,8,9,10,5};
+		System.out.println(ListSumFirst.go(Arrays.asList(one)));
+		Integer[] two = {10,9,8,7,6,5,4,3,2,1,-99};
+		System.out.println(ListSumFirst.go(Arrays.asList(two)));
+		Integer[] three = {10,20,30,40,50,-11818,40,30,20,10};
+		System.out.println(ListSumFirst.go(Arrays.asList(three)));
+		Integer[] four = {32767};
+		System.out.println(ListSumFirst.go(Arrays.asList(four)));
+		Integer[] five = {255,255};
+		System.out.println(ListSumFirst.go(Arrays.asList(five)));
+		Integer[] six = {9,10,-88,100,-555,2};
+		System.out.println(ListSumFirst.go(Arrays.asList(six)));
+		Integer[] seven = {10,10,10,11,456};
+		System.out.println(ListSumFirst.go(Arrays.asList(seven)));
+		Integer[] eight = {-111,1,2,3,9,11,20,1};
+		System.out.println(ListSumFirst.go(Arrays.asList(eight)));
+		Integer[] nine = {9,8,7,6,5,4,3,2,0,-2,6};
+		System.out.println(ListSumFirst.go(Arrays.asList(nine)));
+		Integer[] ten = {12,15,18,21,23,1000};
+		System.out.println(ListSumFirst.go(Arrays.asList(ten)));
+		Integer[] eleven = {250,19,17,15,13,11,10,9,6,3,2,1,0};
+		System.out.println(ListSumFirst.go(Arrays.asList(eleven)));
+		Integer[] tweleve = {9,10,-8,10000,-5000,-3000};
+		System.out.println(ListSumFirst.go(Arrays.asList(tweleve)));
 	}
 }
