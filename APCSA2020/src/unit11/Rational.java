412c661a95fbb575b009885304f8ef8eccacd884
huangp0772 <huangp0772@CA-SU-F106-16.sduhsd.lan>
2020-03-02 11:10:54 -0800

    working

--- APCSA2020/src/unit11/Rational.java
+++ APCSA2020/src/unit11/Rational.java
@@ -8,25 +8,38 @@ import static java.lang.System.*;
 class Rational implements Comparable<Rational>
 {
 	//add two instance variables
-
+	int num1;
+	int num2;
 	//write two constructors
-
-
-	//write a setRational method
+	public Rational() {
+		
+	}
 	
+	public Rational(int numOne,int numTwo) {
+		num1 = numOne;
+		num2 = numTwo;
+	}
+	
+	//write a setRational method
+	public void setRational() {
+		
+	}
 
 	//write  a set method for numerator and denominator
-
+	public void setNumerator(int numOne) {
+		num1 = numOne;
+	}
+	
+	public void setDenominator(int numTwo) {
+		num2 = numTwo;
+	}
+	
 	
 	public void add(Rational  other)
 	{
 		//num1/den1 + num2/den2 
 		//new numerator = (num1 * den2 + num2 * den1)
 		//new denominator = (den1 * den2)
-		
-		
-
-
 		reduce();
 	}
 
@@ -39,8 +52,13 @@ class Rational implements Comparable<Rational>
 
 	private int gcd(int numOne, int numTwo)
 	{
-
-
+		int min = Math.min(numOne,numTwo);
+		for(int x = min; x > 1; x--) {
+			if(numOne%x==0 && numTwo==0) {
+				return x;
+			}
+			return 1;
+		}
 		return 1;
 	}
 
@@ -53,6 +71,12 @@ class Rational implements Comparable<Rational>
 	//ACCESSORS
 
 	//write get methods for numerator and denominator
+	public int getNumerator() {
+		return 1;
+	}
+	public int getDenominator() {
+		return 1;
+	}
 	
 	
 	public boolean equals( Object obj)
@@ -73,6 +97,8 @@ class Rational implements Comparable<Rational>
 
 	
 	//write  toString() method
-	
+	public String toString() {
+		return "";
+	}
 	
 }
