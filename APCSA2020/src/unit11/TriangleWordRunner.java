961bd013c88fe99b767afcec5d7cdc0267348939
huangp0772 <huangp0772@CA-SU-F106-16.sduhsd.lan>
2020-03-02 10:56:17 -0800

    finished

--- APCSA2020/src/unit11/TriangleWordRunner.java
+++ APCSA2020/src/unit11/TriangleWordRunner.java
@@ -10,6 +10,62 @@ public class TriangleWordRunner
 {
 	public static void main( String args[] )
 	{
+		TriangleWord tw = new TriangleWord();
+		Scanner keyboard = new Scanner(System.in);
+		String yesOrNo = "y";
 		
+		while("y".equalsIgnoreCase(yesOrNo)) {
+			System.out.print("Enter a word :: ");
+			String word = keyboard.next();
+			tw.printTriangle(word);
+			
+			System.out.print("\nDo you want to enter more sample input? ");
+			yesOrNo = keyboard.next();
+			System.out.print("\n");
+		}
 	}
 }
+/*------------------------------------result-------------------------------------
+Enter a word :: a
+a
+
+Do you want to enter more sample input? y
+
+Enter a word :: it
+i
+itit
+
+Do you want to enter more sample input? y
+
+Enter a word :: box
+b
+bobo
+boxboxbox
+
+Do you want to enter more sample input? y
+
+Enter a word :: toad
+t
+toto
+toatoatoa
+toadtoadtoadtoad
+
+Do you want to enter more sample input? y
+
+Enter a word :: fishy
+f
+fifi
+fisfisfis
+fishfishfishfish
+fishyfishyfishyfishyfishy
+
+Do you want to enter more sample input? y
+
+Enter a word :: dog
+d
+dodo
+dogdogdog
+
+Do you want to enter more sample input? n
+
+*/
