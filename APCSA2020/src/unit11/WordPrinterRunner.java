2ab29a1f2df8f095e014df0fb3736904c5a71641
huangp0772 <huangp0772@CA-SU-F106-16.sduhsd.lan>
2020-03-02 10:18:35 -0800

    finished

--- APCSA2020/src/unit11/WordPrinterRunner.java
+++ APCSA2020/src/unit11/WordPrinterRunner.java
@@ -1,28 +1,71 @@
 package unit11;
 //(c) A+ Computer Science
+
 //www.apluscompsci.com
 //Name -
 
 import java.util.Scanner;
 import static java.lang.System.*;
 
-public class WordPrinterRunner
-{
-	public static void main( String args[] )
-	{
-   	Scanner keyboard = new Scanner(System.in);
-   	String choice="";
-		do{
-			out.print("\nEnter the word to display :: ");
+public class WordPrinterRunner {
+	public static void main(String args[]) {
+		WordPrinter wp = new WordPrinter();
+		Scanner keyboard = new Scanner(System.in);
+		String choice = "";
+		do {
+			out.print("Enter the word to display :: ");
 			String word = keyboard.next();
-	
-			out.print("Enter the times to display :: ");
- 			int times = keyboard.nextInt();			
-			
-			//call the printWord method
 
+			out.print("Enter the times to display :: ");
+			int times = keyboard.nextInt();
+
+			// call the printWord method
+			wp.printWord(word, times);
 			System.out.print("\nDo you want to enter more sample input? ");
-			choice=keyboard.next();			
-		}while(choice.equals("Y")||choice.equals("y"));		
+			choice = keyboard.next();
+			System.out.print("\n");
+		} while (choice.equals("Y") || choice.equals("y"));
 	}
 }
+/*--------------------------------------result----------------------------------
+Enter the word to display :: hello
+Enter the times to display :: 9
+hello
+hello
+hello
+hello
+hello
+hello
+hello
+hello
+hello
+
+Do you want to enter more sample input? y
+
+Enter the word to display :: funny
+Enter the times to display :: 6
+funny
+funny
+funny
+funny
+funny
+funny
+
+Do you want to enter more sample input? y
+
+Enter the word to display :: chicken
+Enter the times to display :: 4
+chicken
+chicken
+chicken
+chicken
+
+Do you want to enter more sample input? y
+
+Enter the word to display :: dog
+Enter the times to display :: 1
+dog
+
+Do you want to enter more sample input? n
+
+*/
