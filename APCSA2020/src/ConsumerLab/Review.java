d6d1b21edfd0962ca403e77cb76f21357685f045
philliphjhuang <30792325+philliphjhuang@users.noreply.github.com>
2020-03-02 09:46:46 -0800

    Update Review.java

--- APCSA2020/src/ConsumerLab/Review.java
+++ APCSA2020/src/ConsumerLab/Review.java
@@ -36,7 +36,6 @@ public class Review {
 			Scanner input = new Scanner(new File("src/ConsumerLab/positiveAdjectives.txt"));
 			while (input.hasNextLine()) {
 				String temp = input.nextLine().trim();
-				System.out.println(temp);
 				posAdjectives.add(temp);
 			}
 			input.close();
@@ -63,7 +62,7 @@ public class Review {
 	public static String textToString(String fileName) {
 		String temp = "";
 		try {
-			Scanner input = new Scanner(new File("src/ConsumerLab/positiveAdjectives.txt"));
+			Scanner input = new Scanner(new File(fileName));
 
 			// add 'words' in the file to the string, separated by a single space
 			while (input.hasNext()) {
@@ -147,17 +146,21 @@ public class Review {
 		// the filename
 		String contents = textToString(filename);
 		// set up a sentimentTotal variable
-		double sentimentTotal = 0;
-		String word = "";
+		double sentimentTotal = 0.0;
+		int spacePosition = contents.indexOf(' ');
 		// loop through the file contents
 		while(!contents.equals("")) {
 			// find each word
-			int spacePosition = contents.indexOf(' ');
+			if(spacePosition==-1) {
+				sentimentTotal += sentimentVal(contents);
+				contents = "";
+				break;
+			}
 			// add in its sentimentVal
-			
 			sentimentTotal += sentimentVal(contents.substring(0, spacePosition));
 			// set the file contents to start after this word
-			contents = contents.substring(spacePosition+1,contents.length()-1);
+			contents = contents.substring(spacePosition+1, contents.length());
+			spacePosition = contents.indexOf(' ');
 		}
 		return sentimentTotal;
 	}
@@ -168,12 +171,89 @@ public class Review {
 	 */
 	public static int starRating(String filename) {
 		// call the totalSentiment method with the fileName
-
+		double rating = totalSentiment(filename);
 		// determine number of stars between 0 and 4 based on totalSentiment value
 		int stars = 0;
 		// write if statements here
-
+		if(rating<=0) {
+			stars = 0;
+		} else if(rating<=5) {
+			stars = 1;
+		} else if(rating<=10) {
+			stars = 2;
+		} else if(rating<=15) {
+			stars = 3;
+		} else if(rating<=20) {
+			stars = 4;
+		} else if(rating>20) {
+			stars = 5;
+		}
 		// return number of stars
 		return stars;
 	}
+	public static String fakeReview(String filename){
+		String contents = (textToString(filename) + " ");
+		boolean done = true;
+		while (true){
+			done = true;
+			for (int i = 0; i < contents.length(); i ++) {
+				if (contents.charAt(i) == '*') {
+					int nextSpace = contents.substring(i + 1).indexOf(" ") + i + 1;
+					contents = contents.substring(0, i) + randomAdjective() + contents.substring(nextSpace);
+					done = false;
+					break;
+				}
+			}
+			if (done) {
+				return contents;
+			}
+		}
+	}
+	public static String morePositiveReview(String filename){
+		String contents = (textToString(filename) + " ");
+		boolean done = true;
+		while (true){
+			done = true;
+			for (int i = 0; i < contents.length(); i ++) {
+				if (contents.charAt(i) == '*') {
+					int nextSpace = contents.substring(i + 1).indexOf(" ") + i + 1;
+					if (sentimentVal(contents.substring(i + 1, nextSpace)) < 0) {
+						contents = contents.substring(0, i) + randomPositiveAdj() + contents.substring(nextSpace);
+					}
+					else {
+						contents = contents.substring(0, i) + contents.substring(i+ 1);
+					}
+					done = false;
+					break;
+				}
+			}
+			if (done) {
+				return contents;
+			}
+		}
+	}
+	public static String moreNegativeReview(String filename){
+		String contents = (textToString(filename) + " ");
+		boolean done = true;
+		while (true){
+			done = true;
+			for (int i = 0; i < contents.length(); i ++) {
+				if (contents.charAt(i) == '*') {
+					int nextSpace = contents.substring(i + 1).indexOf(" ") + i + 1;
+					if (sentimentVal(contents.substring(i + 1, nextSpace)) > 0) {
+						contents = contents.substring(0, i) + randomNegativeAdj() + contents.substring(nextSpace);
+					}
+					else {
+						contents = contents.substring(0, i) + contents.substring(i+ 1);
+					}
+					done = false;
+					break;
+				}
+			}
+			if (done) {
+				return contents;
+			}
+		}
+	}
+	
 }
