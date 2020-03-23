11634f842078225abda26a1956bfda584a52eb85
philliphjhuang <30792325+philliphjhuang@users.noreply.github.com>
2020-03-23 14:09:11 -0700

    initialized

---
+++ APCSA2020/src/unit13/NumberShifter.java
@@ -0,0 +1,39 @@
+package unit13;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+import static java.lang.System.*;
+
+public class NumberShifter
+{
+	public static int[] makeLucky7Array(int size)
+	{
+		Random rand = new Random();
+		int[] arr = new int[size];
+		for(int i = 0; i < size; i++) {
+			arr[i] = rand.nextInt(10)+1;
+		}
+		return arr;
+	}
+	public static void shiftEm(int[] array)
+	{
+		for(int i = 1; i < array.length; i++) {
+			if(array[i]==7) {
+				for(int j = 0; j < i; j++) {
+					if(array[j]!=7) {
+						int temp = array[j];
+						array[j] = array[i];
+						array[i] = temp;
+						break;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ APCSA2020/src/unit13/NumberShifterRunner.java
@@ -0,0 +1,36 @@
+package unit13;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import static java.lang.System.*;
+
+public class NumberShifterRunner
+{
+	public static void main( String args[] ) throws IOException
+	{
+		NumberShifter ns = new NumberShifter();
+		//add test cases
+		int[] array1 = ns.makeLucky7Array(10);
+		System.out.println(Arrays.toString(array1));
+		ns.shiftEm(array1);
+		System.out.println(Arrays.toString(array1));
+		
+		int[] array2 = ns.makeLucky7Array(11);
+		System.out.println("\n\n" + Arrays.toString(array2));
+		ns.shiftEm(array2);
+		System.out.println(Arrays.toString(array2));
+		
+		int[] array3 = ns.makeLucky7Array(12);
+		System.out.println("\n\n" + Arrays.toString(array3));
+		ns.shiftEm(array3);
+		System.out.println(Arrays.toString(array3));
+	}
+}
+
+
+
