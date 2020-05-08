7fbf90261d1aa6c36e939a5c744f92eef2779a27
philliphjhuang <30792325+philliphjhuang@users.noreply.github.com>
2020-05-07 23:42:32 -0700

    .

--- APCSA2020/src/WinterScene/SnowMan.java
+++ APCSA2020/src/WinterScene/SnowMan.java
@@ -18,6 +18,10 @@ public class SnowMan extends AbstractShape {
 		window.fillOval(super.getXPos() + 20, super.getYPos() - 50, super.getWidth() - 25, super.getHeight() - 25);
 		window.fillOval(super.getXPos() + 10, super.getYPos() + 15, super.getWidth(), super.getHeight());
 		window.fillOval(super.getXPos() - 10, super.getYPos() + 100, super.getWidth() + 40, super.getHeight() + 40);
+		window.setColor(Color.black);
+		window.fillOval(super.getXPos() + 35, super.getYPos() - 35, super.getWidth() - 85, super.getHeight() - 85);
+		window.fillOval(super.getXPos() + 65, super.getYPos() - 35, super.getWidth() - 85, super.getHeight() - 85);
+		window.fillOval(super.getXPos() + 50, super.getYPos() - 5, super.getWidth() - 85, super.getHeight() - 85);
 	}
 
 	public void moveAndDraw(Graphics window) {
